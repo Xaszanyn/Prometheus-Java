af60dc4fec8861cd960ba523816f92e92cc18f75
klcrsln <kilicarslansoyler3@gmail.com>
2023-05-28 01:35:41 +0300

    sAÇMA SAPAN ŞEYLER.

--- src/main/java/com/bitirmetezi/prometheusjava/controller/alertcontroller/AlertApi.java
+++ src/main/java/com/bitirmetezi/prometheusjava/controller/alertcontroller/AlertApi.java
@@ -6,7 +6,6 @@ import com.bitirmetezi.prometheusjava.service.alertservice.AlertServiceOutput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
