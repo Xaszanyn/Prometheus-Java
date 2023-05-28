ba1e67eb972dbd6e7956f857250f3c18acba78fe
klcrsln <kilicarslansoyler3@gmail.com>
2023-05-29 02:42:26 +0300

    Services and controllers created.

--- src/main/java/com/bitirmetezi/prometheusjava/controller/alerthistorycontroller/HistoryApi.java
+++ src/main/java/com/bitirmetezi/prometheusjava/controller/alerthistorycontroller/HistoryApi.java
@@ -3,10 +3,11 @@ package com.bitirmetezi.prometheusjava.controller.alerthistorycontroller;
 import com.bitirmetezi.prometheusjava.controller.BaseResponse;
 import com.bitirmetezi.prometheusjava.service.alerthistoryservice.HistoryService;
 import com.bitirmetezi.prometheusjava.service.alerthistoryservice.HistoryServiceOutput;
-import com.bitirmetezi.prometheusjava.service.alertservice.AlertServiceOutput;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
